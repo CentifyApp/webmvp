0fdbc1262b2d57d5fc1f5dedd04489628c31ca03
Fred Hua <40416564+fredhua233@users.noreply.github.com>
2023-07-15 15:20:51 -0400

    updated stripe prices

--- web/lib/globals.dart
+++ web/lib/globals.dart
@@ -3,27 +3,27 @@ library globals;
 import 'package:web/models/playerInfo.dart';
 
 Map<int, String> prices = {
-  5: "price_1N3rUpFrHOOVbovuWKGMP0VQ",
-  10: "price_1N3rUpFrHOOVbovudBfpjJaN",
-  15: "price_1N3rUpFrHOOVbovuXJ0ktv3J",
-  20: "price_1N3rUpFrHOOVbovu8gpfZgrk",
-  3: "price_1N3rUpFrHOOVbovuCa711ucJ",
-  2: "price_1N3rUpFrHOOVbovuaq0VgLwg",
-  4: "price_1N3rUpFrHOOVbovuoOO3A0Wj",
-  6: "price_1N3rUpFrHOOVbovuI4PJqdhA",
-  7: "price_1N3rUpFrHOOVbovuPZkjrvzM",
-  8: "price_1N3rUpFrHOOVbovuh7LR4ZRP",
-  9: "price_1N3rUpFrHOOVbovuWydniKPK",
-  1: "price_1N3rUpFrHOOVbovuENQZNYF2",
-  25: "price_1N3rUpFrHOOVbovumQPcNlrK",
-  30: "price_1N3rUpFrHOOVbovuylZ0kEAf",
-  40: "price_1N3rUpFrHOOVbovucenMu4KH",
-  50: "price_1N3rUpFrHOOVbovuF8NesrR6",
-  60: "price_1N3rUpFrHOOVbovu17kmdUuh",
-  70: "price_1N3rUpFrHOOVbovufxtk86Fy",
-  80: "price_1N3rUpFrHOOVbovuHVUzFMXF",
-  90: "price_1N3rUpFrHOOVbovu96885ksa",
-  100: "price_1N3rUpFrHOOVbovuwIZ6gm3m"
+  5: "price_1NUDiADNgwsz7mTdqbXWvSo1",
+  10: "price_1NUDiADNgwsz7mTd4S9uq2UY",
+  15: "price_1NUDiADNgwsz7mTdvplhuVXU",
+  20: "price_1NUDiADNgwsz7mTdmyJVe90E",
+  3: "price_1NUDiADNgwsz7mTdEvJEh5tv",
+  2: "price_1NUDiADNgwsz7mTdB7OioTWO",
+  4: "price_1NUDiADNgwsz7mTdJjRvP4Rn",
+  6: "price_1NUDiADNgwsz7mTdLLiGfbHY",
+  7: "price_1NUDiADNgwsz7mTd42RAUn76",
+  8: "price_1NUDiADNgwsz7mTdxkUO5mXM",
+  9: "price_1NUDiBDNgwsz7mTdgtIbFjHo",
+  1: "price_1NUDiADNgwsz7mTdjeQz9AvK",
+  25: "price_1NUDiADNgwsz7mTdtAGdpfbS",
+  30: "price_1NUDiADNgwsz7mTdZdFGV1U9",
+  40: "price_1NUDiADNgwsz7mTdspqguooV",
+  50: "price_1NUDiBDNgwsz7mTdrnUySkrR",
+  60: "price_1NUDiBDNgwsz7mTdSvGMI2Q8",
+  70: "price_1NUDiBDNgwsz7mTdonlzqzGx",
+  80: "price_1NUDiBDNgwsz7mTdS1KJS1S4",
+  90: "price_1NUDiBDNgwsz7mTdNbEcjGXp",
+  100: "price_1NUDiBDNgwsz7mTdQRjaFAX4"
 };
 
 List<int> priceList = [

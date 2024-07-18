91d5f617b0c523430dd0669d34872e099a3e1116
Fred Hua <40416564+fredhua233@users.noreply.github.com>
2024-07-18 19:50:15 -0400

    bye

--- web/lib/main.dart
+++ web/lib/main.dart
@@ -93,8 +93,6 @@ class _MyHomePageState extends State<MyHomePage> {
   Future<UserCredential> signInWithGoogle() async {
     GoogleAuthProvider googleProvider = GoogleAuthProvider();
 
-    googleProvider
-        .addScope('https://www.googleapis.com/auth/contacts.readonly');
     googleProvider.setCustomParameters({'login_hint': 'user@example.com'});
 
     return await FirebaseAuth.instance.signInWithPopup(googleProvider);
